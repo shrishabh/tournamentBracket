ee8afaae9adfc152684232658ff201b5df8003a4
Rishabh Khandelwal <rkhandelwal@Rishabh.eau.wi.charter.com>
2018-04-22 09:56:14 -0500

    added a background image, fixed the exception that was showing on exit

--- application/Main.java
+++ application/Main.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -18,6 +19,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 
@@ -28,107 +30,115 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
+			 //Testing a piece of code - Rishabh
+//		Screen screen = Screen.getPrimary();
+//        Rectangle2D bounds = screen.getVisualBounds();
+//
+//        primaryStage.setX(bounds.getMinX());
+//        primaryStage.setY(bounds.getMinY());
+//        primaryStage.setWidth(bounds.getWidth());
+//        primaryStage.setHeight(bounds.getHeight());
+		 Label teams[] = new Label[list.size()];
+		 for(int i = 0; i<teams.length; i++)
+		 {				
+			 teams[i]= new Label(list.get(i).getName());
+			 
+		 }
 
-			 Label teams[] = new Label[list.size()];
-			 for(int i = 0; i<teams.length; i++)
-			 {				
-				 teams[i]= new Label(list.get(i).getName());
-				 
-			 }
+		 VBox teamsScore[] = new VBox[teams.length];
+		 for(int i = 0; i<teamsScore.length; i++)
+		 {
+			 teamsScore[i] = new VBox();
+			 teamsScore[i].getChildren().addAll(teams[i], new TextField());
+		 }
+		GridPane grid = new GridPane();
+		grid.setId("pane");
+		primaryStage.setTitle("Tournament Bracket");
 
-			 VBox teamsScore[] = new VBox[teams.length];
-			 for(int i = 0; i<teamsScore.length; i++)
-			 {
-				 teamsScore[i] = new VBox();
-				 teamsScore[i].getChildren().addAll(teams[i], new TextField());
-			 }
-			 GridPane grid = new GridPane();
-			primaryStage.setTitle("Tournament Bracket");
+		 if(teams.length == 1)
+		 {
+			 grid.add(teamsScore[0], 0, 0);
+		 }
+		 else if(teams.length == 2)
+		 {
+			 grid.add(teamsScore[0], 0, 0);
+			 grid.add(new Button("Submit Scores"), 0, 1);
+			 grid.add(teamsScore[1], 0, 2);
+		 }
+		 else if(teams.length == 4)
+		 {
+			 grid.add(teamsScore[0], 0, 0);
+			 grid.add(new Button("Submit Scores"), 0, 1);
+			 grid.add(teamsScore[3], 0, 2);
+			 grid.add(teamsScore[1], 0, 3);
+			 grid.add(new Button("Submit Scores"), 0, 4);
+			 grid.add(teamsScore[2], 0, 5);
+		 }
+		 else if(teams.length == 8)
+		 {
+			 grid.add(teamsScore[0], 0, 0);
+			 grid.add(new Button("Submit Scores"), 0, 1);
+			 grid.add(teamsScore[7], 0, 2);
+			 grid.add(teamsScore[3], 0, 3);
+			 grid.add(new Button("Submit Scores"), 0, 4);
+			 grid.add(teamsScore[4], 0, 5);
+			 grid.add(teamsScore[1], 0, 6);
+			 grid.add(new Button("Submit Scores"), 0, 7);
+			 grid.add(teamsScore[6], 0, 8);
+			 grid.add(teamsScore[2], 0, 9);
+			 grid.add(new Button("Submit Scores"), 0, 10);
+			 grid.add(teamsScore[5], 0, 11);
+		 }
+		 else if(teams.length == 16)
+		 {
+			 
+		 }
+		 else if(teams.length == 32)
+		 {
+			 
+		 }
+		 Scene scene = new Scene(grid, 1500, 800, Color.DARKGRAY);
+		 scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+		 //primaryStage.setScene(scene);
+		 //primaryStage.show();
 
-			 if(teams.length == 1)
-			 {
-				 grid.add(teamsScore[0], 0, 0);
-			 }
-			 else if(teams.length == 2)
-			 {
-				 grid.add(teamsScore[0], 0, 0);
-				 grid.add(new Button("Submit Scores"), 0, 1);
-				 grid.add(teamsScore[1], 0, 2);
-			 }
-			 else if(teams.length == 4)
-			 {
-				 grid.add(teamsScore[0], 0, 0);
-				 grid.add(new Button("Submit Scores"), 0, 1);
-				 grid.add(teamsScore[3], 0, 2);
-				 grid.add(teamsScore[1], 0, 3);
-				 grid.add(new Button("Submit Scores"), 0, 4);
-				 grid.add(teamsScore[2], 0, 5);
-			 }
-			 else if(teams.length == 8)
-			 {
-				 grid.add(teamsScore[0], 0, 0);
-				 grid.add(new Button("Submit Scores"), 0, 1);
-				 grid.add(teamsScore[7], 0, 2);
-				 grid.add(teamsScore[3], 0, 3);
-				 grid.add(new Button("Submit Scores"), 0, 4);
-				 grid.add(teamsScore[4], 0, 5);
-				 grid.add(teamsScore[1], 0, 6);
-				 grid.add(new Button("Submit Scores"), 0, 7);
-				 grid.add(teamsScore[6], 0, 8);
-				 grid.add(teamsScore[2], 0, 9);
-				 grid.add(new Button("Submit Scores"), 0, 10);
-				 grid.add(teamsScore[5], 0, 11);
-			 }
-			 else if(teams.length == 16)
-			 {
-				 
-			 }
-			 else if(teams.length == 32)
-			 {
-				 
-			 }
-			 Scene scene = new Scene(grid, 1500, 800, Color.DARKGRAY);
-			 /////////
-				primaryStage.setScene(scene);
-				primaryStage.show();
+		VBox vbox = new VBox(10);
+		
+		
+		Label label = new Label();
+		label.setAlignment(Pos.CENTER);
+		label.setMinHeight(25);
+		label.setText("Team 1 ");
+		
+		Label label2 = new Label();
+		label2.setAlignment(Pos.CENTER);
+		label2.setMinHeight(25);
+		label2.setText("Team 2 ");
+		
+		TextField input = new TextField();
+		TextField input2 = new TextField();
+		input.setMaxHeight(20); input.setMaxWidth(75);
+		input2.setMaxHeight(20); input2.setMaxWidth(75);
+		input.setPromptText("Score");
+		input2.setPromptText("Score");
+		input.setFocusTraversable(false);
+		input2.setFocusTraversable(false);
+		
+		
+		Button submitButton = new Button();
+		submitButton.setText("Submit");
+		submitButton.setOnAction(new EventHandler<ActionEvent>() {
 
-			VBox vbox = new VBox(10);
-			
-			
-			Label label = new Label();
-			label.setAlignment(Pos.CENTER);
-			label.setMinHeight(25);
-			label.setText("Team 1 ");
-			
-			Label label2 = new Label();
-			label2.setAlignment(Pos.CENTER);
-			label2.setMinHeight(25);
-			label2.setText("Team 2 ");
-			
-			TextField input = new TextField();
-			TextField input2 = new TextField();
-			input.setMaxHeight(20); input.setMaxWidth(75);
-			input2.setMaxHeight(20); input2.setMaxWidth(75);
-			input.setPromptText("Score");
-			input2.setPromptText("Score");
-			input.setFocusTraversable(false);
-			input2.setFocusTraversable(false);
-			
-			
-			Button submitButton = new Button();
-			submitButton.setText("Submit");
-			submitButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				int score1 = Integer.parseInt(input.getText());
+				int score2 = Integer.parseInt(input.getText()); // TODO fix this
+				
+				System.out.println(score1>score2);
+			}
+		});
 
-				@Override
-				public void handle(ActionEvent event) {
-					int score1 = Integer.parseInt(input.getText());
-					int score2 = Integer.parseInt(input.getText()); // TODO fix this
-					
-					System.out.println(score1>score2);
-				}
-			});
-	
-			vbox.getChildren().addAll(label, label2, input, input2, submitButton);
+		vbox.getChildren().addAll(label, label2, input, input2, submitButton);
 
 
 			
@@ -200,7 +210,7 @@ public class Main extends Application {
 			System.out.println("Please enter valid number of teams");
 			System.exit(-1);
 		}
-		launch(args);
+		//launch(args);
 
 	}
 }
