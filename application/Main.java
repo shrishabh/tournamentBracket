b2215d13e7e5b77447ddb64d19645f24e96ea57e
Patrick <phwang4@wisc.edu>
2018-04-30 22:15:31 -0500

    adding more button stuff

--- application/Main.java
+++ application/Main.java
@@ -196,7 +196,11 @@ public class Main extends Application {
 	                     int score2 = Integer.parseInt(t2.getText());
 	                     list.get(matchupPos[pos]).setScore(score);
 	                     list.get(matchupPos[pos+1]).setScore(score2);
-	                     winnerList.add(getWinner(list.get(matchupPos[pos]), list.get(matchupPos[pos+1])));
+	                     Challenger winner = getWinner(list.get(matchupPos[pos]), list.get(matchupPos[pos+1]));
+	                     System.out.println(teamsScore[matchupPos[pos]].getChildren().get(0));
+	                     if (!winnerList.contains(winner)) // if someone wants to submit a different score
+	                     winnerList.add(winner);           // then the same winner won't be added multiple times
+	                                                       // TODO if the winner changes, replace in winnerList
 	                     System.out.println(score + " " + score2);
 	                 }
 	             });
